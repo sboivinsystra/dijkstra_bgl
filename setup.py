490fb5415fdb46fb9b79b557245c64f04353f68d
Simon Boivin <sboivin@systra.com>
2025-12-11 10:00:08 -0500

    change boost dir and add windows parallel flag in setup

--- setup.py
+++ setup.py
@@ -1,16 +1,33 @@
 from setuptools import setup, Extension
 import pybind11
+import platform
 import os
 
-boost_include = os.environ.get("BOOST_INCLUDEDIR", "")
+boost_include = os.environ.get("BOOST_INCLUDEDIR", r"C:\local\boost_1_86_0")
+
+
+# Define OpenMP flags based on the compiler
+def get_openmp_flags():
+    system = platform.system()
+    if system == "Windows":
+        return ["/openmp"], []  # Compile flags, Link flags
+    elif system == "Darwin":  # macOS (Apple Clang) is tricky with OpenMP
+        # You might need 'libomp' installed via brew
+        return ["-Xpreprocessor", "-fopenmp"], ["-lomp"]
+    else:  # Linux / GCC
+        return ["-fopenmp"], ["-fopenmp"]
+
+
+omp_compile_args, omp_link_args = get_openmp_flags()
+
 ext_modules = [
     Extension(
         "pyboostgraph",
         ["pyboostgraph.cpp"],
         include_dirs=[pybind11.get_include(), boost_include],
         language="c++",
-        extra_compile_args=["-O3", "-fopenmp"],  # Enable OpenMP
-        extra_link_args=["-fopenmp"],  # Link OpenMP library
+        extra_compile_args=omp_compile_args,
+        extra_link_args=omp_link_args,
     ),
 ]
 
