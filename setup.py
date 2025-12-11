76ec64d8545e3b24889e1f2b272fd16c5e77c03d
Simon Boivin <sboivin@systra.com>
2025-12-11 15:06:07 -0500

    remove env for real

--- setup.py
+++ setup.py
@@ -3,13 +3,18 @@ import pybind11
 import platform
 import os
 
+system = platform.system()
 boost_include = os.environ.get("BOOST_INCLUDEDIR", "")
-print("boost_include: ", boost_include)
+
+
+include_dirs = [pybind11.get_include()]
+if system == "Windows":
+    print("boost_include: ", boost_include)
+    include_dirs.append(boost_include)
 
 
 # Define OpenMP flags based on the compiler
 def get_openmp_flags():
-    system = platform.system()
     if system == "Windows":
         return ["/openmp"], []  # Compile flags, Link flags
     elif system == "Darwin":  # macOS (Apple Clang) is tricky with OpenMP
@@ -25,7 +30,7 @@ ext_modules = [
     Extension(
         "pyboostgraph",
         ["pyboostgraph.cpp"],
-        include_dirs=[pybind11.get_include(), boost_include],
+        include_dirs=include_dirs,
         language="c++",
         extra_compile_args=omp_compile_args,
         extra_link_args=omp_link_args,
